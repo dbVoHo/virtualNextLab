009530053bf8254b5ef37ae214b76a56ddbe207a
Volker Hoff <volker.hoff-extern@deutschebahn.com>
2022-04-04 09:29:15 +0200

    teamN

---
+++ Images.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Images" tilewidth="652" tileheight="590" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="2">
+  <image width="652" height="590" source="src/assets/KleyerstraßeA.png"/>
+ </tile>
+</tileset>
